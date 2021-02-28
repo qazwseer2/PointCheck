903408605dbbeb54a15303d78b5c397d014690a1
qazwseer2 <56082845+qazwseer2@users.noreply.github.com>
2021-02-28 11:48:38 +0300

    Kyurem Forms/Color formatting

--- Item.js
+++ Item.js
@@ -52,237 +52,240 @@ var mega = {
     "pixelmon:audinite":"audino",
     //Gen6
     "pixelmon:diancite":"diancie"
-    }
+}
 var SpecI = {
     "Blue Orb":"kyogre",
     "Red Orb":"groudon"
-    }
+}
 var PointList = {
-"alakazam-m":1,
-"alomomola":1,
-"amoonguss":1,
-"azumarill":1,
-"charizard-mx":1,
-"charizard-my":1,
-"cloyster":1,
-"conkeldurr":1,
-"darmanitan":1,
-"dragonite":1,
-"excadrill":1,
-"gallade-m":1,
-"garchomp":1,
-"garchomp-m":1,
-"gardevoir-m":1,
-"gligar":1,
-"gliscor":1,
-"gothitelle":1,
-"grimmsnarl":1,
-"gyarados-m":1,
-"hatterene":1,
-"hawlucha":1,
-"heracross-m":1,
-"kommo-o":1,
-"indeedee":1,
-"klefki":1,
-"lopunny-m":1,
-"magnezone":1,
-"mawile-m":1,
-"mimikyu":1,
-"pinsir-m":1,
-"porygon2":1,
-"pyukumuku":1,
-"quagsire":1,
-"rillaboom":1,
-"rotom-heat":1,
-"serperior":1,
-"scolipede":1,
-"shuckle":1,
-"slurpuff":1,
-"slowbro-g":1,
-"slowking":1,
-"snorlax":1,
-"swampert-m":1,
-"sylveon":1,
-"tyranitar":1,
-"umbreon":1,
-"volcarona":1,
-"weezing-g":1,
-"wobbuffet":1,
-"buzzwole":1,
-"nihilego":1,
-"cobalion":1,
-"cresselia":1,
-"keldeo":1,
-"latias":1,
-"latios":1,
-"moltres":1,
-"suicune":1,
-"terrakion":1,
-"tornadus":1,
-"thundurus":1,
-"thundurus-t":1,
-"volcanion":1,
-"zeraora":1,
+    "alakazam-m":1,
+    "alomomola":1,
+    "amoonguss":1,
+    "azumarill":1,
+    "charizard-mx":1,
+    "charizard-my":1,
+    "cloyster":1,
+    "conkeldurr":1,
+    "darmanitan":1,
+    "dragonite":1,
+    "excadrill":1,
+    "gallade-m":1,
+    "garchomp":1,
+    "garchomp-m":1,
+    "gardevoir-m":1,
+    "gligar":1,
+    "gliscor":1,
+    "gothitelle":1,
+    "grimmsnarl":1,
+    "gyarados-m":1,
+    "hatterene":1,
+    "hawlucha":1,
+    "heracross-m":1,
+    "kommo-o":1,
+    "indeedee":1,
+    "klefki":1,
+    "lopunny-m":1,
+    "magnezone":1,
+    "mawile-m":1,
+    "mimikyu":1,
+    "pinsir-m":1,
+    "porygon2":1,
+    "pyukumuku":1,
+    "quagsire":1,
+    "rillaboom":1,
+    "rotom-heat":1,
+    "serperior":1,
+    "scolipede":1,
+    "shuckle":1,
+    "slurpuff":1,
+    "slowbro-g":1,
+    "slowking":1,
+    "snorlax":1,
+    "swampert-m":1,
+    "sylveon":1,
+    "tyranitar":1,
+    "umbreon":1,
+    "volcarona":1,
+    "weezing-g":1,
+    "wobbuffet":1,
+    "buzzwole":1,
+    "nihilego":1,
+    "cobalion":1,
+    "cresselia":1,
+    "keldeo":1,
+    "latias":1,
+    "latios":1,
+    "moltres":1,
+    "suicune":1,
+    "terrakion":1,
+    "tornadus":1,
+    "thundurus":1,
+    "thundurus-t":1,
+    "volcanion":1,
+    "zeraora":1,
 
-"aegislash":2,
-"blacephalon":2,
-"blaziken":2, 
-"celesteela":2,
-"blissey":2,
-"kartana":2,
-"cinderace":2,
-"xurkitree":2,
-"clefable":2,
-"deoxys-def":2,
-"corsola-g":2,
-"diancie-m":2,
-"corviknight":2,
-"heatran":2,
-"darmanitan-g":2,
-"hoopa-unbound":2,
-"ditto":2,
-"jirachi":2,
-"dragapult":2,
-"kyurem-black":2,
-"gengar-m":2,
-"landorus":2,
-"greninja-ash":2,
-"landorus-t":2,
-"lucario-m":2,   
-"magearna":2,
-"kangaskhan-m":2,
-"melmetal":2,
-"medicham-m":2,
-"mew":2,
-"metagross-m":2,
-"tapubulu":2,
-"rotom-wash":2,
-"tapufini":2,
-"scizor-m":2,
-"tapukoko":2,
-"slowbro":2,
-"slowbro-m":2,
-"tapulele":2,
-"skarmory":2,
-"tornadus-t":2,
-"smeargle":2,
-"victini":2,
-"tangrowth":2,
-"zapdos":2,
-"venusaur-m":2,
-"zygarde-50%":2,
+    "aegislash":2,
+    "blacephalon":2,
+    "blaziken":2, 
+    "celesteela":2,
+    "blissey":2,
+    "kartana":2,
+    "cinderace":2,
+    "xurkitree":2,
+    "clefable":2,
+    "deoxys-def":2,
+    "corsola-g":2,
+    "diancie-m":2,
+    "corviknight":2,
+    "heatran":2,
+    "darmanitan-g":2,
+    "hoopa-unbound":2,
+    "ditto":2,
+    "jirachi":2,
+    "dragapult":2,
+    "kyurem-black":2,
+    "gengar-m":2,
+    "landorus":2,
+    "greninja-ash":2,
+    "landorus-t":2,
+    "lucario-m":2,   
+    "magearna":2,
+    "kangaskhan-m":2,
+    "melmetal":2,
+    "medicham-m":2,
+    "mew":2,
+    "metagross-m":2,
+    "tapubulu":2,
+    "rotom-wash":2,
+    "tapufini":2,
+    "scizor-m":2,
+    "tapukoko":2,
+    "slowbro":2,
+    "slowbro-m":2,
+    "tapulele":2,
+    "skarmory":2,
+    "tornadus-t":2,
+    "smeargle":2,
+    "victini":2,
+    "tangrowth":2,
+    "zapdos":2,
+    "venusaur-m":2,
+    "zygarde-50%":2,
 
-"chansey":3,
-"toxapex":3,
-"ferrothorn":3,
-"sableye-m":3,
-"salamence-m":3,
-"naganadel":3,
-"pheromosa":3,
-"darkrai":3,
-"deoxys-speed":3,
-"deoxys":3,
-"diagla":3,
-"genesect":3,
-"giratina":3,
-"kyurem-white":3,
-"lugia":3,
-"lunala":3,
-"palkia":3,
-"reshiram":3,
-"shaymin-sky":3,
-"solgaleo":3,
-"zekrom":3,
+    "chansey":3,
+    "toxapex":3,
+    "ferrothorn":3,
+    "sableye-m":3,
+    "salamence-m":3,
+    "naganadel":3,
+    "pheromosa":3,
+    "darkrai":3,
+    "deoxys-speed":3,
+    "deoxys":3,
+    "diagla":3,
+    "genesect":3,
+    "giratina":3,
+    "kyurem-white":3,
+    "lugia":3,
+    "lunala":3,
+    "palkia":3,
+    "reshiram":3,
+    "shaymin-sky":3,
+    "solgaleo":3,
+    "zekrom":3,
 
-"deoxys-attack":4,
-"groudon":4,
-"ho-oh":4,
-"marshadow":4,
-"mewtwo":4,
-"mewtwo-m":4,
-"necrozma-dawn":4,
-"necrozma-dusk":4,
-"rayquaza":4,
+    "deoxys-attack":4,
+    "groudon":4,
+    "ho-oh":4,
+    "marshadow":4,
+    "mewtwo":4,
+    "mewtwo-m":4,
+    "necrozma-dawn":4,
+    "necrozma-dusk":4,
+    "rayquaza":4,
 
-"arceus":5,
-"kyogre":5,
-"kyogre-primal":5,
-"necrozma-ultra":5,
-"xerneas":5,
-"yveltal":5,
-"zygarde-complete":5,
-"zygarde-100%":5,
+    "arceus":5,
+    "kyogre":5,
+    "kyogre-primal":5,
+    "necrozma-ultra":5,
+    "xerneas":5,
+    "yveltal":5,
+    "zygarde-complete":5,
+    "zygarde-100%":5,
 
-"groudon-primal":6,
-"rayquaza-m":6
+    "groudon-primal":6,
+    "rayquaza-m":6
 }
 function interact(e) {
-var party = e.player.pixelmonData.party.		getAll()
-var team = []
-var points = 0
-for(var i = 0; i < e.player.pixelmonData.party.		countAblePokemon(); i++) {
-team[i] = party[i].getBaseStats().pixelmonName.toLowerCase()
-for (var r = 0; r < party[i].getMoveset().length;r++){
-if (String(party[i].getMoveset()[r]) == "Dragon Ascent" && party[i].getBaseStats().pixelmonName.toLowerCase() == "rayquaza") {
-team[i] = team[i]+"-m"
-}
-if (String(party[i].getMoveset()[r]) == "Baton Pass") {
-points++
-}
-if (String(party[i].getMoveset()[r]) == "Minimize") {
-team[i] = team[i]+"(ILLEGAL:Minimize)"
-}
-if (String(party[i].getMoveset()[r]) == "Double Team") {
-team[i] = team[i]+"(ILLEGAL:Double Team)"
-}
-}
-if (party[i].	getAbilityName() == "Moody") {
-team[i] = team[i]+"(ILLEGAL:Moody)"
-}
-if (party[i].	getAbilityName() == "Moody") {
-team[i] = team[i]+"(ILLEGAL:Moody)"
-}
-if (party[i].getBaseStats().pixelmonName.toLowerCase() == "necrozma" && party[i].getBaseStats().form == 2) {
-team[i] = team[i]+"-dawn"
-}
-if (party[i].getBaseStats().pixelmonName.toLowerCase() == "necrozma" && party[i].getBaseStats().form == 1) {
-team[i] = team[i]+"-dusk"
-}
-if ((String(party[i].getHeldItemAsItemHeld().getRegistryName()) in mega)==true && (mega[String(party[i].getHeldItemAsItemHeld().getRegistryName())]== party[i].getBaseStats().pixelmonName.toLowerCase())) {
-team[i] = team[i]+"-m"
-}
-if ((String(party[i].getHeldItemAsItemHeld().getRegistryName()) in SpecI)==true && (SpecI[String(party[i].getHeldItemAsItemHeld().getRegistryName())]== party[i].getBaseStats().pixelmonName.toLowerCase())) {
-team[i] = team[i]+"-primal"
-}
-if (party[i].form == 4 && party[i].getBaseStats().pixelmonName.toLowerCase() == "rotom" ) {
-team[i] = team[i]+"-heat"
-}
-if (party[i].form == 2 && party[i].getBaseStats().pixelmonName.toLowerCase() == "rotom" ) {
-team[i] = team[i]+"-wash"
-}
-if (party[i].form == 2 && party[i].getBaseStats().pixelmonName.toLowerCase() == "slowbro" || party[i].getBaseStats().pixelmonName.toLowerCase() == "weezing" || party[i].getBaseStats().pixelmonName.toLowerCase() == "corsola" || party[i].getBaseStats().pixelmonName.toLowerCase() == "darmanitan") {
-team[i] = team[i]+"-g"
-}
-e.player.message(party[i].form)
-if (party[i].form == 1 && party[i].getBaseStats().pixelmonName.toLowerCase() == "hoopa" ) {
-team[i] = team[i]+"-unbound"
-}
-if (party[i].form == 1 && party[i].getBaseStats().pixelmonName.toLowerCase() == "landorus" || party[i].getBaseStats().pixelmonName.toLowerCase() == "thundurus" || party[i].getBaseStats().pixelmonName.toLowerCase() == "tornadus" ) {
-team[i] = team[i]+"-t"
-}
-}
-e.player.message(team)
+    var party = e.player.pixelmonData.party.getAll()
+    var team = []
+    var points = 0
+    for(var i = 0; i < e.player.pixelmonData.party.countAblePokemon(); i++) {
+        team[i] = party[i].getBaseStats().pixelmonName.toLowerCase()
+        for (var r = 0; r < party[i].getMoveset().length;r++) {
+            if (String(party[i].getMoveset()[r]) == "Dragon Ascent" && party[i].getBaseStats().pixelmonName.toLowerCase() == "rayquaza") {
+                team[i] = team[i]+"-m"
+            }
+            if (String(party[i].getMoveset()[r]) == "Baton Pass") {
+                points++
+            }
+            if (String(party[i].getMoveset()[r]) == "Minimize") {
+                team[i] = team[i]+"(ILLEGAL:Minimize)"
+            }
+            if (String(party[i].getMoveset()[r]) == "Double Team") {
+                team[i] = team[i]+"(ILLEGAL:Double Team)"
+            }
+        }
+        if (party[i].getAbilityName() == "Moody") {
+            team[i] = team[i]+"(ILLEGAL:Moody)"
+        }
+        if (party[i].getAbilityName() == "Moody") {
+            team[i] = team[i]+"(ILLEGAL:Moody)"
+        }
+        if (party[i].getBaseStats().pixelmonName.toLowerCase() == "necrozma" && party[i].getBaseStats().form == 2) {
+            team[i] = team[i]+"-dawn"
+        }
+        if (party[i].getBaseStats().pixelmonName.toLowerCase() == "necrozma" && party[i].getBaseStats().form == 1) {
+            team[i] = team[i]+"-dusk"
+        }
+        if ((String(party[i].getHeldItemAsItemHeld().getRegistryName()) in mega) == true && (mega[String(party[i].getHeldItemAsItemHeld().getRegistryName())]== party[i].getBaseStats().pixelmonName.toLowerCase())) {
+            team[i] = team[i]+"-m"
+        }
+        if ((String(party[i].getHeldItemAsItemHeld().getRegistryName()) in SpecI) ==  true && (SpecI[String(party[i].getHeldItemAsItemHeld().getRegistryName())]== party[i].getBaseStats().pixelmonName.toLowerCase())) {
+            team[i] = team[i]+"-primal"
+        }
+        if (party[i].form == 4 && party[i].getBaseStats().pixelmonName.toLowerCase() == "rotom" ) {
+            team[i] = team[i]+"-heat"
+        }
+        if (party[i].form == 2 && party[i].getBaseStats().pixelmonName.toLowerCase() == "rotom" ) {
+            team[i] = team[i]+"-wash"
+        }
+        if (party[i].form == 2 && party[i].getBaseStats().pixelmonName.toLowerCase() == "slowbro" || party[i].getBaseStats().pixelmonName.toLowerCase() == "weezing" || party[i].getBaseStats().pixelmonName.toLowerCase() == "corsola" || party[i].getBaseStats().pixelmonName.toLowerCase() == "darmanitan") {
+            team[i] = team[i]+"-g"
+        }
+        if (party[i].form == 1 && party[i].getBaseStats().pixelmonName.toLowerCase() == "hoopa" ) {
+            team[i] = team[i]+"-unbound"
+        }
+        if (party[i].form == 1 && party[i].getBaseStats().pixelmonName.toLowerCase() == "landorus" || party[i].getBaseStats().pixelmonName.toLowerCase() == "thundurus" || party[i].getBaseStats().pixelmonName.toLowerCase() == "tornadus" ) {
+            team[i] = team[i]+"-t"
+        }
+        if (party[i].form == 2 && party[i].getBaseStats().pixelmonName.toLowerCase() == "kyurem" ) {
+            team[i] = team[i]+"-white"
+        }
+        if (party[i].form == 1 && party[i].getBaseStats().pixelmonName.toLowerCase() == "kyurem" ) {
+            team[i] = team[i]+"-black"
+        }
+    }
+    e.player.message("§c§l"+team)
 
-
-
-for (var i = 0; i < team.length; i++){
-if ((team[i] in PointList) == true) {
-e.player.message(team[i]+" - "+PointList[team[i]]+ " очков")
-points =points+ PointList[team[i]]
-}
-if ((team[i] in PointList) == false) {
-e.player.message(team[i]+" - 0 очков или неверное название покемона")
-}
-}
-e.player.message("Всего - "+points+" очков")
+    for (var i = 0; i < team.length; i++) {
+        if ((team[i] in PointList) == true) {
+            e.player.message("§e§l"+team[i]+"§a§l - §b§l" + PointList[team[i]] + "§a§l очков")
+            points =points+ PointList[team[i]]
+        }
+        if ((team[i] in PointList) == false) {
+            e.player.message("§e§l"+team[i]+"§a§l - §b§l0§a§l очков или неверное название покемона")
+        }
+    }
+    e.player.message("§6§lВсего - §4§l" + points + "§6§l очков")
 }
